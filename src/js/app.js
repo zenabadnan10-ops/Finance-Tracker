ddc81351439cbfcc45aa2347fee5530fec6bc458
zenabadnan10-ops <zenabadnan10@gmail.com>
2026-09-17 23:28:42 +0500

    add js for deleting transaction

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,42 @@
 import {renderNavigation} from "./ui/switch-sections.js";
-import { closeTransactionModal, getForm, openTransactionModal, resetForm } from "./ui/modals.js";
-import { addTransaction, editTransaction, getTransaction } from "./transactions/transactions.js";
+import { closeDeleteModal, closeTransactionModal, getForm, openDeleteModal, openTransactionModal, resetForm } from "./ui/modals.js";
+import { addTransaction, deleteTransaction, editTransaction, getTransaction } from "./transactions/transactions.js";
 import { renderTransactions } from "./transactions/transactionUI.js";
 import { states } from "./state/state.js";
 import { validateForm } from "./transactions/validation.js";
 import { loadData } from "./storage/localStorage.js";
 
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+
+window.scrollTo(0, 0);
+
 const addTransactionBtns = document.querySelectorAll(".add-transaction-btn");
+const closeBtn = document.getElementById("close-transaction-btn");
+const deleteModalBtn = document.getElementById("delete-modal-btn");
 const cancelBtn = document.getElementById("cancel-add-btn");
 const transactionList = document.getElementById("transactions-cards-section");
 
 const setupEventListeners = () => {
+
+    closeBtn.addEventListener("click", () => {
+        closeDeleteModal();
+        states.ui.deletingTransactionId = null;
+    });
+
+    deleteModalBtn.addEventListener("click", () => {
+        const id = states.ui.deletingTransactionId;
+
+        if(id) {
+            deleteTransaction(id);
+            renderTransactions(states.transactions);
+            states.ui.deletingTransactionId = null;
+        }
+
+        closeDeleteModal();
+
+    });
 
     addTransactionBtns.forEach(btn => {
         btn.addEventListener("click", () =>{
@@ -27,19 +53,32 @@ const setupEventListeners = () => {
 
     transactionList.addEventListener("click", (e) => {
         const editBtn = e.target.closest('[data-action="edit"]');
+        const deleteBtn = e.target.closest('[data-action="delete"]');
 
-        const id = editBtn.dataset.id;
-        const transaction = getTransaction(id);
+        if(editBtn) {
 
-        states.ui.editingTransactionId = id;
-        openTransactionModal(transaction);
-    })
+            const id = editBtn.dataset.id;
+            const transaction = getTransaction(id);
+    
+            states.ui.editingTransactionId = id;
+            openTransactionModal(transaction);
+
+        }
+
+        if(deleteBtn) {
+            const id = deleteBtn.dataset.id;
+
+            states.ui.deletingTransactionId = id;
+            openDeleteModal();
+        }
+
+    });
 
     getForm().addEventListener("submit", (e) => {
         e.preventDefault();
 
         handleSubmission();
-    })
+    });
 
 };
 
